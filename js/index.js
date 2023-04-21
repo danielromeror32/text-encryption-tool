01a69453777a206e34f8e267ed4ebf28d8172a43
danielromeror32 <daniel321789@gmail.com>
2023-04-21 03:28:04 -0600

    Adding functions encrypt and descrypt to JS

--- js/index.js
+++ js/index.js
@@ -1,32 +1,117 @@
 const buttonEncip = document.querySelector("#firstButton");
 const buttonDesc = document.querySelector("#secondtButton");
+const buttonCopy = document.querySelector(".buttom-decrypt");
 
-let textEncryption = "Ningún mensaje fue encontrado";
+let textEncryption ;
 let textDecryption ;
+let textCopied;
 
 // button event Encryption
 buttonEncip.addEventListener("click", function() {
-    var textEncryption = getText(); 
+    textEncryption = getText("textarea"); 
+    textDecryption = setText(textEncryption);
   });
-// button event Decryption
-  buttonDesc.addEventListener("click", function() {
-    var textDecryption = setText(textEncryption); 
-    
+// button event copy
+buttonCopy.addEventListener("click", function() {
+  textCopied = getTextLabel(".displayText");
+  // alert(textCopied);
 });
+// button event Decryption
+buttonDesc.addEventListener("click", function() {
+  // textDecryption = setText(textEncryption); 
+  textDecryption = decryptionWord(textCopied); // console
+  textEncryption = setTextLabel(textDecryption);
+});
+
+
+
 
 
 // Function for pass text to second panel 
 function setText (text){
-    document.querySelector(".displayText").textContent =text;
+    //Encriptar palabra 
+    let wordEncript = encryptionWord(text);
+    // set word 
+    document.querySelector(".displayText").textContent = wordEncript;
 }
 
 
  // Function extraer el texto ingresado en el 1er panel 
-function getText (){
-    let extractText = document.querySelector("textarea"); // Select content to textarea
+function getText(area){
+    let extractText = document.querySelector(area); // Select content to textarea
     var text = extractText.value;
+    extractText.value = '';
     // alert(text);
-    return text;
+    return text.toLowerCase();
 }
 
 
+function getTextLabel (area){
+  let extractText = document.querySelector(area);
+  var text = extractText.textContent;
+  return text;
+}
+function setTextLabel (area){
+
+  document.querySelector(".displayText").textContent = area;
+  
+}
+
+
+
+
+function encryptionWord  (palabra){
+  
+  let newTextArray = [];
+  // let textArray = textEncryption.split("");
+  let textArray = palabra.split("");
+  
+  for (let index = 0; index < textArray.length; index++){
+    
+    let element = textArray[index];
+   
+    if (element === "e"){
+      newTextArray.push("enter");
+    }
+    else if (element === "i"){
+      newTextArray.push("imes");
+    }
+    else if (element === "a"){
+      newTextArray.push("ai");
+    }
+    else if (element === "o"){
+      newTextArray.push("ober");
+    }
+    else if (element === "u"){
+      newTextArray.push("ufat");
+    }else{
+      newTextArray.push(element);
+    }
+
+  }
+  let completeWord = newTextArray.join(""); 
+  // console.log(completeWord);
+  return (completeWord);
+}
+
+
+
+const dictionary = {
+  "e": "enter", 
+  "i": "imes",
+  "a": "ai",
+  "o": "ober",
+  "u": "ufat"
+}
+
+function decryptionWord (word){
+  let palabra = word;
+  // palabra = palabra.replace("ai", "a");
+  // palabra = palabra.replace("ober", "o");
+ 
+  for (let property in dictionary){
+    palabra = palabra.replace(dictionary[property], property);
+    // console.log((dictionary[property]+ ": " +  property));
+  }
+  console.log(palabra);
+}
